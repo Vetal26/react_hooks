26cab5baf6c06fe4ffc35708102eae2463e376a0
Vitaly Borisov <vitaliy.borisov@intspirit.com>
2021-10-18 13:14:45 +0300

    useRef

---
+++ src/example/ref.js
@@ -0,0 +1,37 @@
+import { useState, useEffect, useRef } from "react";
+
+function App() {
+  // const [renderCount, setRenderCount] = useState(1);
+  const [value, setValue] = useState("initial");
+  const renderCount = useRef(1);
+  const inputRef = useRef(null);
+  const prevValue = useRef("");
+
+  useEffect(() => {
+    renderCount.current++;
+  });
+
+  useEffect(() => {
+    prevValue.current = value;
+  }, [value]);
+
+  const focus = () => inputRef.current.focus();
+
+  return (
+    <div className="App">
+      <h1>Counter: {renderCount.current}</h1>
+      <h2>Prev: {prevValue.current}</h2>
+      <input
+        ref={inputRef}
+        type="text"
+        onChange={(e) => setValue(e.target.value)}
+        value={value}
+      />
+      <button className="btn btn-success" onClick={focus}>
+        Focus
+      </button>
+    </div>
+  );
+}
+
+export default App;
