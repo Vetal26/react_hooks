1a5c484d0f04e2a0720044e6b69196666f46e607
Vitaly Borisov <vitaliy.borisov@intspirit.com>
2021-10-19 12:00:26 +0300

    useReducer

--- src/Main.js
+++ src/Main.js
@@ -2,12 +2,12 @@ import React from "react";
 import { useAlert } from "./alert/AlertContext";
 
 export default function Main() {
-  const { toggle } = useAlert();
+  const { show } = useAlert();
 
   return (
     <div>
       <h1>Пример с Context</h1>
-      <button onClick={toggle} className="btn btn-success">
+      <button onClick={() => show("text")} className="btn btn-success">
         Показать alert
       </button>
     </div>
--- src/alert/AlertContext.js
+++ src/alert/AlertContext.js
@@ -1,32 +1,45 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import { useContext } from "react";
 
 const AlertContext = React.createContext();
-// const AlertToggleContext = React.createContext();
 
 export const useAlert = () => {
   return useContext(AlertContext);
 };
 
-// export const useToggleAlert = () => {
-//   return useContext(AlertToggleContext);
-// };
+const SHOW_ALERT = "show";
+const HIDE_ALERT = "hide";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case SHOW_ALERT:
+      return { ...state, visible: true, text: action.text };
+    case HIDE_ALERT:
+      return { ...state, visible: false };
+    default:
+      return state;
+  }
+};
 
 export const AlertProvider = ({ children }) => {
-  const [alert, setAlert] = useState(false);
+  const [state, dispatch] = useReducer(reducer, {
+    visible: false,
+    text: "",
+  });
 
-  const toggle = () => setAlert((prev) => !prev);
+  const show = (text) => dispatch({ type: SHOW_ALERT, text });
+  const hide = () => dispatch({ type: HIDE_ALERT });
 
   return (
     <AlertContext.Provider
       value={{
-        visible: alert,
-        toggle,
+        visible: state.visible,
+        text: state.text,
+        show,
+        hide,
       }}
     >
-      {/* <AlertToggleContext.Provider value={toggle}> */}
       {children}
-      {/* </AlertToggleContext.Provider> */}
     </AlertContext.Provider>
   );
 };
