b932a175dfb7db10e108a117de0a9b1fbdd9f5eb
Vitaly Borisov <vitaliy.borisov@intspirit.com>
2021-10-19 12:27:17 +0300

    creating hook

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
-import React from "react";
-import Alert from "./alert/Alert";
-import Main from "./Main";
-import { AlertProvider } from "./alert/AlertContext";
+import React, { useEffect, useState } from "react";
+
+function useLogger(value) {
+  useEffect(() => {}, [value]);
+}
+
+function useInput(initialValue) {
+  const [value, setValue] = useState(initialValue);
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const clear = () => setValue("");
+
+  return {
+    bind: { value, onChange },
+    value,
+    clear,
+  };
+}
 
 function App() {
+  const input = useInput("");
+
+  useLogger(input.value);
+
   return (
-    <AlertProvider>
-      <div className="container pt-3">
-        <Alert />
-        <Main toggle={() => {}} />
-      </div>
-    </AlertProvider>
+    <div className={"container pt-3"}>
+      <input type="text" {...input.bind} />
+      <button className="btn btn-warning" onClick={() => input.clear()}>
+        Очистить
+      </button>
+      <hr />
+      <h1>{input.value}</h1>
+    </div>
   );
 }
 
